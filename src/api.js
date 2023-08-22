80cd8da184a48a5a3fda0868412c456f8ad858e8
Bishnu Mishra <mishravishnu369@gmail.com>
2023-08-23 04:51:51 +0530

    made autocomplete search comp

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b3324fb2a4msh37716898d661f73p15fb7bjsn595c5c65c6e1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,27 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({onSearchChange}) => {
+    
     const [search, setSearch] = useState(null);
+
+    const loadOptions = async (inputValue) => {
+		try {
+			const response = await fetch(`${GEO_API_URL}/cities?=minPopulation=100&namePrefix=${inputValue}`, geoApiOptions);
+			const result = await response.json();
+		
+			const options = result.data.map((city) => ({
+				value: `${city.latitude} ${city.longitude}`,
+				label: `${city.name}, ${city.countryCode}`,
+			}));
+			return { options };
+		} catch (error) {
+			console.error(error);
+			return { options: [] }; // You might want to handle the error more gracefully
+		}
+	};
+	  
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
@@ -11,9 +30,13 @@ const Search = ({onSearchChange}) => {
 
     return (
         <AsyncPaginate
-            placeholder="Search a city" debounceTimeout={500} value={search} onChange={handleOnChange}
+            placeholder="Search a city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
-    )
+    );
 }
 
 export default Search;
