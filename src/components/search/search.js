b96f670064e04e27866bb980e00dae1e821e711a
Bishnu Mishra <mishravishnu369@gmail.com>
2023-08-22 20:04:56 +0530

    made basic Search bar, i.e Search comp

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,19 @@
-const Search = () => {
-    return ('Hello');
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = ({onSearchChange}) => {
+    const [search, setSearch] = useState(null);
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate
+            placeholder="Search a city" debounceTimeout={500} value={search} onChange={handleOnChange}
+        />
+    )
 }
 
 export default Search;
