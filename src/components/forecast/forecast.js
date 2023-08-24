9e156d9196f75604e31bc2fd89edb424fdc896c4
Bishnu Mishra <mishravishnu369@gmail.com>
2023-08-25 03:07:22 +0530

    added Accordion comp to forecast.js

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,5 +1,24 @@
-const Forecast = () => {
-    return "hello";
+import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemPanel, AccordionItemButton } from "react-accessible-accordion";
+
+const Forecast = ({data}) => {
+    return (
+        <>
+            <label className="title">Daily</label>
+            <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, index) => (
+                    <AccordionItem key={index}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                Hello
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
+                ))}
+                
+            </Accordion>
+        </>
+    );
 }
 
 export default Forecast;
