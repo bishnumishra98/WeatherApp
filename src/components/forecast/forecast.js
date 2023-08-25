aea4602fb4a71fee54a8263ead7beba325740ae8
Bishnu Mishra <mishravishnu369@gmail.com>
2023-08-25 20:56:36 +0530

    added img icons in accordion

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,6 +1,13 @@
 import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemPanel, AccordionItemButton } from "react-accessible-accordion";
 
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 const Forecast = ({data}) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+    console.log(forecastDays);
+
     return (
         <>
             <label className="title">Daily</label>
@@ -9,16 +16,18 @@ const Forecast = ({data}) => {
                     <AccordionItem key={index}>
                         <AccordionItemHeading>
                             <AccordionItemButton>
-                                Hello
+                                <div className="daily-item">
+                                    <img src={`icons/${item.weather[0].icon}.png`} alt="weather-icon" className="icon-small" />
+                                    <label className="day"></label>
+                                </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
                         <AccordionItemPanel></AccordionItemPanel>
                     </AccordionItem>
-                ))}
-                
+                ))}  
             </Accordion>
         </>
     );
-}
+};
 
 export default Forecast;
